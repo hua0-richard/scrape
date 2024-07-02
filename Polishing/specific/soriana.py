4be1b47176a96d56b8f59b7e3b87f3be9df7f8b0
Richard Hua <r8hua@uwaterloo.ca>
2024-07-02 09:39:46 -0400

    changes

--- Polishing/specific/soriana.py
+++ Polishing/specific/soriana.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 nutrition_label_mappings_soriana = {
     "Energía por porción" : "Cals_value_pp",
-    "Carbohydrates" : "TotalCarb_g_pp",
+    "Carbohidratos" : "TotalCarb_g_pp",
     "Azúcares Totales": "TotalSugars_g_pp"
 }
 
@@ -17,8 +17,9 @@ def basics(input, index, clean):
             if key in l:
                 tmp = l
                 val = tmp.replace(key, "").strip()
-                val = re.sub(r'\D', '', val)
+                #val = re.sub(r'\D', '', val)
                 clean.loc[index, nutrition_label_mappings_soriana[key]] = val
+                
                 if (nutrition_label_mappings_soriana[key] == 'Cals_value_pp'):
                     clean.loc[index, 'Cals_unit_pp'] = 'kcal'
 
@@ -30,11 +31,15 @@ def p100g(input, index, clean, type_a, type_b):
     for l in lines:
         if net_content in l:
             tmp = l
+            clean.loc[index, 'Netcontent_org'] = tmp
             val = tmp.replace(net_content, "").strip()
+            val_unit = re.sub(r'[^a-zA-Z]', '', val)
             val = re.sub(r'[^0-9.]', '', val)
             net_content_num = float(val)
-            clean.loc[index, type_a] =  (net_content_num / 100) * float(clean.loc[index, type_b])
+            nums = re.sub(r'\D', '', clean.loc[index, type_b])
+            clean.loc[index, type_a] =  (net_content_num / 100) * float(nums)
             clean.loc[index, 'Netcontent_val'] = net_content_num
+            clean.loc[index, 'Netcontent_unit'] = val_unit
             return
 
 
@@ -80,6 +85,7 @@ def container_size(input, index, clean):
             val = tmp.replace(servings_per_container, "").strip()
             val = re.sub(r'[^0-9.]', '', val)
             servings_per_container_val = float(val)
+            clean.loc[index, 'Servings_cont'] = servings_per_container_val
         if serving_size in l:
             tmp = l
             val = tmp.replace(serving_size, "").strip()
@@ -91,6 +97,16 @@ def container_size(input, index, clean):
     if (serving_size_val > 0 and servings_per_container_val > 0):
         clean.loc[index, 'Containersize_val'] = contval
         clean.loc[index, 'Containersize_unit'] = 'ml'
+
+def parse_from_name(index, clean, dirty):
+    tmp = dirty.loc[index, 'name']
+    pattern = re.compile(r'\b\d+(\.\d+)?\s*(ml|l|L|g|kg|oz|Ml|Lt|fl oz)\b')
+    matches = pattern.search(tmp)
+    if (matches):
+        val = re.sub(r'[^0-9.]', '', matches[0])
+        clean.loc[index, 'Containersize_val'] = val
+        val = re.sub(r'[^a-zA-Z]', '', matches[0])
+        clean.loc[index, 'Containersize_unit'] = val
         
 
             
@@ -102,11 +118,26 @@ class Soriana(mappings):
         container_type(input = dirty.loc[index, 'item_label'], index = index, clean = clean)
         container_size(input = dirty.loc[index, 'item_label'], index = index, clean = clean)
 
+        if (pd.isnull(clean.loc[index, 'Containersize_val'])):
+            parse_from_name(index = index, clean = clean, dirty = dirty)
+
         clean.loc[index, 'NutrInfo_org'] = dirty.loc[index, 'item_label']
-        if (pd.isnull(clean.loc[index, 'TotalCarb_g_pp']) == False):
-            p100g(input = dirty.loc[index, 'item_label'], index = index, clean = clean, type_a = 'TotalCarb_g_pp', type_b = 'TotalCarb_g_p100g')
-        if (pd.isnull(clean.loc[index, 'TotalSugars_g_pp']) == False):
-            p100g(input = dirty.loc[index, 'item_label'], index = index, clean = clean, type_a = 'TotalSugars_g_pp', type_b = 'TotalSugars_g_p100g')
+        clean.loc[index, 'NutrLabel'] = dirty.loc[index, 'item_label']
+
+        # if (pd.isnull(clean.loc[index, 'TotalCarb_g_pp']) == False):
+        #     p100g(input = dirty.loc[index, 'item_label'], index = index, clean = clean, type_a = 'TotalCarb_g_p100g', type_b = 'TotalCarb_g_pp')
+        # if (pd.isnull(clean.loc[index, 'TotalSugars_g_pp']) == False):
+        #     p100g(input = dirty.loc[index, 'item_label'], index = index, clean = clean, type_a = 'TotalSugars_g_p100g', type_b = 'TotalSugars_g_pp')
+        
+        @staticmethod
+        def country(index, clean):
+            clean.loc[index, 'Country'] = 'Mexico'
+        @staticmethod
+        def indices(index, clean):
+            INDEX = 30
+            clean.loc[index, 'ID'] = 30
+
 
         
         
+
