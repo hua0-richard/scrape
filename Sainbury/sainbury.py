66b45670476d3e5b993d8f7138d3cf293904b339
Richard Hua <r8hua@uwaterloo.ca>
2024-08-15 12:06:45 -0400

    Sainbury

--- Sainbury/sainbury.py
+++ Sainbury/sainbury.py
@@ -27,14 +27,65 @@ import random
 import re
 
 FAVNUM = 22222
+GEN_TIMEOUT = 5
 
 def setup_sainbury(driver, EXPLICIT_WAIT_TIME, site_location_df, ind, url):
     setLocation_sainbury(driver, site_location_df.loc[ind, 1], EXPLICIT_WAIT_TIME)
 
 
 def setLocation_sainbury(driver, address, EXPLICIT_WAIT_TIME):
-    
-    print('Set Location Complete')
+    # Reject Cookies Button
+    try:
+        WebDriverWait(driver, EXPLICIT_WAIT_TIME).until(
+            EC.element_to_be_clickable((By.ID, "onetrust-reject-all-handler"))
+        ).click()
+        print('Reject Cookies Button')
+    except Exception as e:
+        print('No Cookies Button')
+
+    # Login
+    for _ in range(5):
+        try:
+            time.sleep(GEN_TIMEOUT)
+            WebDriverWait(driver, EXPLICIT_WAIT_TIME).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "a.top-right-links--login[data-test-id='login-register-link']"))
+            ).click()
+            print('Login Button')
+            break
+        except Exception as e:
+            print(f'Error Clicking Login Button. Trying Again. Attempt {_}')
+
+    # Reject Cookies Button Login
+    try:
+        WebDriverWait(driver, EXPLICIT_WAIT_TIME).until(
+            EC.element_to_be_clickable((By.ID, "onetrust-reject-all-handler"))
+        ).click()
+        print('Reject Cookies Button')
+    except Exception as e:
+        print('No Cookies Button')
+
+    # Login Data
+    text_enter = WebDriverWait(driver, EXPLICIT_WAIT_TIME).until(
+        EC.visibility_of_element_located((By.ID, 'username'))
+    )
+    text_enter.send_keys('u2894478@gmail.com')
+
+    text_enter = WebDriverWait(driver, EXPLICIT_WAIT_TIME).until(
+        EC.visibility_of_element_located((By.ID, 'password'))
+    )
+    text_enter.send_keys('Notme123!')
+
+    try:
+        WebDriverWait(driver, EXPLICIT_WAIT_TIME).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='log-in']"))).click()
+        print('Set Location Complete')
+        print('Found Login Button')
+    except Exception as e:
+        print('Did not find Login Button')
+
+    input('Continue? [Y] / [N]')
+
+    time.sleep(FAVNUM)
+
 
 
 def scrapeSite_sainbury(driver, EXPLICIT_WAIT_TIME=10, idx=None, aisles=[], ind=None,
