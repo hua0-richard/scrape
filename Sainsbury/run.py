1999f4dc71cd03f09faff4e072148b8b73f7753d
Richard Hua <richard.hua@uwaterloo.ca>
2024-08-16 08:35:12 -0400

    Data Needs To be Fixed Sainsbury

--- Sainsbury/run.py
+++ Sainsbury/run.py
@@ -12,7 +12,7 @@ def main():
     EXPLICIT_WAIT_TIME = 10
     site_location_df = pd.read_excel('urlLocations.xlsx', header=None)
 
-    for _ in [66,67,68,69]:
+    for _ in [65, 66, 67, 68]:
         ind = _
         print('\nIndex: ', ind)
         url = site_location_df.loc[ind, 0]
@@ -39,30 +39,30 @@ def main():
 
         driver.quit()
 
-        for _ in [66, 67, 68, 69]:
-            ind = _
-            print('\nIndex: ', ind)
-            url = site_location_df.loc[ind, 0]
-            scrape_location = site_location_df.loc[ind, 1]
-            scrape_country = site_location_df.loc[ind, 2]
-            scrape_store = site_location_df.loc[ind, 3]
-            print(url)
-            print(scrape_location)
-            print(scrape_country)
-            print(scrape_store)
-            ind = _ + 1
+    for _ in [65, 66, 67, 68]:
+        ind = _
+        print('\nIndex: ', ind)
+        url = site_location_df.loc[ind, 0]
+        scrape_location = site_location_df.loc[ind, 1]
+        scrape_country = site_location_df.loc[ind, 2]
+        scrape_store = site_location_df.loc[ind, 3]
+        print(url)
+        print(scrape_location)
+        print(scrape_country)
+        print(scrape_store)
+        ind = _ + 1
 
-            driver_options = uc.chrome.options.Options()
-            driver_options.add_argument("--disable-notifications")
-            driver_options.add_argument("--disable-infobars")
-            driver_options.add_argument("--disable-extensions")
-            driver = uc.Chrome(driver_options)
-            driver.maximize_window()
+        driver_options = uc.chrome.options.Options()
+        driver_options.add_argument("--disable-notifications")
+        driver_options.add_argument("--disable-infobars")
+        driver_options.add_argument("--disable-extensions")
+        driver = uc.Chrome(driver_options)
+        driver.maximize_window()
 
-            driver.get(url)
+        driver.get(url)
 
-            sainsbury.setup_sainbury(driver, EXPLICIT_WAIT_TIME, site_location_df, ind, url)
-            sainsbury.scrapeSite_sainbury(driver, EXPLICIT_WAIT_TIME, idx=str(ind), aisle='Dairy, eggs & chilled',ind=ind)
+        sainsbury.setup_sainbury(driver, EXPLICIT_WAIT_TIME, site_location_df, ind, url)
+        sainsbury.scrapeSite_sainbury(driver, EXPLICIT_WAIT_TIME, idx=str(ind), aisle='Dairy, eggs & chilled',ind=ind)
 
 if __name__ == '__main__':
     res = main()
