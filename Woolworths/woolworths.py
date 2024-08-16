04c262b91fc42fb7925a4ad47d61ba9aa1709303
Richard Hua <r8hua@uwaterloo.ca>
2024-08-16 10:37:25 -0400

    Woolworths Skeleton Code

---
+++ Woolworths/woolworths.py
@@ -0,0 +1,111 @@
+from selenium.webdriver.common.by import By
+import pandas as pd
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import urllib.request
+from bs4 import BeautifulSoup
+
+# Special Package to scrap and aids in avoiding more stringent sites
+import undetected_chromedriver as uc
+
+import time
+import datetime
+import pytz
+
+import random
+
+import re
+
+FAVNUM = 22222
+GEN_TIMEOUT = 5
+STORE_NAME = 'woolworths'
+
+def setup_woolworths(driver, EXPLICIT_WAIT_TIME, site_location_df, ind, url):
+    setLocation_woolworths(driver, site_location_df.loc[ind - 1, 1], EXPLICIT_WAIT_TIME)
+
+
+def setLocation_woolworths(driver, address, EXPLICIT_WAIT_TIME):
+    # Reject Cookies Button
+    print('Set Location Complete')
+
+def scrapeSite_woolworths(driver, EXPLICIT_WAIT_TIME, idx=None, aisle='', ind=None):
+    subaisles = []
+    items = []
+
+    # check for previous items
+    try:
+        items = pd.read_csv(f"output/tmp/index_{str(ind)}_{aisle}_item_urls.csv")
+        items = items.iloc[:, 0].tolist()
+        print('Found Prior Items')
+    except Exception as e:
+
+        pd.DataFrame(items).to_csv(f'output/tmp/index_{str(ind)}_{aisle}_item_urls.csv', index=False, header=None,encoding='utf-8-sig')
+        print(f'items so far... {len(items)}')
+
+    # check for previous scrape
+    df_data = pd.DataFrame()
+    site_items_df = pd.DataFrame()
+    try:
+        df_data = pd.read_csv(f"output/tmp/index_{str(ind)}_{aisle}_{STORE_NAME}_data.csv")
+        site_items_df = pd.concat([site_items_df, df_data], ignore_index=True).drop_duplicates()
+    except:
+        print('No Prior Data Found... ')
+
+    # scrape items and check for already scraped
+    for item_index in range(len(items)):
+        item_url = items[item_index]
+        if not df_data.empty and items[item_index] in df_data['url'].values:
+            print(f'{ind}-{item_index} Item Already Exists!')
+            continue
+
+        for v in range(5):
+            try:
+                time.sleep(GEN_TIMEOUT)
+                driver.get(item_url)
+                new_row = scrape_item(driver, aisle, item_url, EXPLICIT_WAIT_TIME, ind, item_index)
+                site_items_df = pd.concat([site_items_df, pd.DataFrame([new_row])], ignore_index=True)
+                site_items_df = site_items_df.drop_duplicates(subset=['url'], keep='last')
+                print(new_row)
+                break
+            except Exception as e:
+                print(f'Failed to scrape item. Attempt {v}. Trying Again... ')
+                print(e)
+
+        if (item_index % 10 == 0):
+            site_items_df.to_csv(f'output/tmp/index_{str(ind)}_{aisle}_{STORE_NAME}_data.csv', index=False)
+
+    site_items_df.to_csv(f'output/tmp/index_{str(ind)}_{aisle}_{STORE_NAME}_data.csv', index=False)
+
+def scrape_item(driver, aisle, item_url, EXPLICIT_WAIT_TIME, ind, index):
+    itemIdx = f'{ind}-{index}-{aisle.upper()[:3]}'
+    name = None
+    brand = None
+    subaisle = None
+    subsubaisle = None
+    size = None
+    price = None
+    multi_price = None
+    old_price = None
+    pricePerUnit = None
+    itemNum = None
+    description = None
+    serving = None
+    img_urls = []
+    item_label = None
+    item_ingredients = None
+    pack = None
+
+    new_row = {'idx': itemIdx,
+               'name': name, 'brand': brand,
+               'aisle': aisle, 'subaisle': subaisle,
+               'subsubaisle': subsubaisle,
+               'size': size, 'price': price, 'multi_price': multi_price,
+               'old_price': old_price, 'pricePerUnit': pricePerUnit,
+               'itemNum': itemNum, 'description': description, 'serving': serving,
+               'img_urls': ', '.join(img_urls), 'item_label': item_label,
+               'item_ingredients': item_ingredients, 'url': item_url,
+               'pack': pack,
+               'timeStamp': datetime.datetime.now(pytz.timezone('US/Eastern')).isoformat()}
+    return (new_row)
