13948c7739640131e00d91e888f3960a4697d839
Richard Hua <r8hua@uwaterloo.ca>
2024-09-10 22:03:12 -0400

    Kroger

--- Kroger/kroger.py
+++ Kroger/kroger.py
@@ -1,73 +1,247 @@
-from selenium.webdriver.common.by import By
+import glob
+import os
+import requests
 import pandas as pd
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 import time
 import datetime
 import pytz
-import re
 
 FAVNUM = 22222
+GEN_TIMEOUT = 6
+STORE_NAME = 'target'
+LOCATION = ''
+MAX_RETRY = 10
 
-def setup_kroger(driver, EXPLICIT_WAIT_TIME, site_location_df, ind, url):
-    setLocation_kroger(driver, site_location_df.loc[ind, 1], EXPLICIT_WAIT_TIME)
+def custom_sort_key(value):
+    parts = value.split('-')
+    return int(parts[1])
+
+def cache_strategy():
+    folder_path = 'output/tmp'
+    item_urls_csvs = glob.glob(os.path.join(folder_path, '*data.csv'))
+    reference_df_list = []
+    for file in item_urls_csvs:
+        df = pd.read_csv(file)
+        reference_df_list.append(df)
+    combined_reference_df = pd.concat(reference_df_list, ignore_index=True)
+    combined_reference_df.drop_duplicates(subset='url', keep='first', inplace=True)
+    return combined_reference_df
+
+
+def setup_target(driver, EXPLICIT_WAIT_TIME, site_location_df, ind, url):
+    setLocation_kroger(driver, site_location_df.loc[ind - 1, 1], EXPLICIT_WAIT_TIME)
 
 
 def setLocation_kroger(driver, address, EXPLICIT_WAIT_TIME):
-    None
+    global LOCATION
+    LOCATION = address
+    time.sleep(GEN_TIMEOUT)
+
     print('Set Location Complete')
 
+def scrapeSite_kroger(driver, EXPLICIT_WAIT_TIME, idx=None, aisle='', ind=None):
+    items = []
+    try:
+        items = pd.read_csv(f"output/tmp/index_{str(ind)}_{aisle}_item_urls.csv")
+        items = items.iloc[:, 0].tolist()
+        print('Found Prior Items')
+    except Exception as e:
+        print('No Prior Items')
 
-def scrapeSite_kroger(driver, EXPLICIT_WAIT_TIME=10, idx=None, aisles=[], ind=None,
-                      site_location_df=None):
-    #  Setup Data
-    site_items_df = pd.DataFrame(columns=['idx', 'name', 'brand', 'aisle', 'subaisle', 'subsubaisle',
-                                          'size', 'price', 'multi_price',
-                                          'old_price', 'pricePerUnit', 'itemNum',
-                                          'description', 'serving', 'img_urls',
-                                          'item_label', 'item_ingredients',
-                                          'pack',
-                                          'url', 'timeStamp'])
+        pd.DataFrame(items).to_csv(f'output/tmp/index_{str(ind)}_{aisle}_item_urls.csv', index=False, header=None, encoding='utf-8-sig')
+        print(f'items so far... {len(items)}')
 
-    for aisle in aisles:
-        None
-        # site_items_df.to_csv(f'output/tmp/index_{str(ind)}_{aisle}_soriana_data.csv', index=False)
+    df_data = pd.DataFrame()
+    site_items_df = pd.DataFrame()
+
+    # Cache Strategy
+    try:
+        seen_items = cache_strategy()
+        new_rows = []
+        for cache_index in range(len(items)):
+            item_url = items[cache_index]
+            print(f"{cache_index} x {item_url}")
+            matching_rows = seen_items[seen_items['url'] == item_url]
+            if len(matching_rows) > 0:
+                row = matching_rows.iloc[0].copy()
+                row['ID'] = f'{ind}-{cache_index}-{aisle.upper()[:3]}'
+                index_for_here = f'{ind}-{cache_index}-{aisle.upper()[:3]}'
+                print(f'Found Cached Entry {cache_index}')
+                new_rows.append(row)
+                try:
+                    full_path = 'output/images/' + str(ind) + '/' + str(row['ID']) + '/' + str(
+                        index_for_here) + '-' + str(0) + '.png'
+                    if not os.path.isfile(full_path):
+                        response = requests.get(row['img_urls'])
+                        if response.status_code == 200:
+                            with open(full_path, 'wb') as file:
+                                file.write(response.content)
+                except:
+                    print('Images Error Cache')
+        if new_rows:
+            new_rows_df = pd.DataFrame(new_rows)
+            df_data = pd.concat([df_data, new_rows_df], ignore_index=True)
+            df_data = df_data.drop_duplicates(subset=['url'], keep='last')
+            site_items_df = pd.concat([site_items_df, df_data], ignore_index=True).drop_duplicates()
+            site_items_df = site_items_df.sort_values(by='ID', key=lambda x: x.map(custom_sort_key))
+            site_items_df = site_items_df.reset_index(drop=True)
+    except Exception as e:
+        print(e)
+        print('Cache Failed')
+
+    # scrape items and check for already scraped
+    try:
+        df_data = pd.read_csv(f"output/tmp/index_{str(ind)}_{aisle}_{STORE_NAME}_data.csv")
+        site_items_df = pd.concat([site_items_df, df_data], ignore_index=True).drop_duplicates()
+    except:
+        print('No Prior Data Found... ')
+
+    for item_index in range(len(items)):
+        item_url = items[item_index]
+        print(item_url)
+        if not df_data.empty and items[item_index] in df_data['url'].values:
+            print(f'{ind}-{item_index} Item Already Exists!')
+            continue
+
+        for v in range(5):
+            try:
+                time.sleep(GEN_TIMEOUT)
+                driver.get(item_url)
+                new_row = scrape_item(driver, aisle, item_url, EXPLICIT_WAIT_TIME, ind, item_index)
+                if new_row is None:
+                    break
+                site_items_df = pd.concat([site_items_df, pd.DataFrame([new_row])], ignore_index=True)
+                site_items_df = site_items_df.drop_duplicates(subset=['url'], keep='last')
+                print(new_row)
+                break
+            except Exception as e:
+                print(f'Failed to scrape item. Attempt {v}. Trying Again... ')
+                print(e)
+
+        if (item_index % 10 == 0):
+            site_items_df.to_csv(f'output/tmp/index_{str(ind)}_{aisle}_{STORE_NAME}_data.csv', index=False)
+
+    site_items_df.to_csv(f'output/tmp/index_{str(ind)}_{aisle}_{STORE_NAME}_data.csv', index=False)
 
 
 def scrape_item(driver, aisle, item_url, EXPLICIT_WAIT_TIME, ind, index):
-    itemIdx = None
-    name = None
-    brand = None
-    subaisle = None
-    subsubaisle = None
-    size = None
-    price = None
-    multi_price = None
-    old_price = None
-    pricePerUnit = None
-    itemNum = None
-    description = None
-    serving = None
-    img_urls = None
-    item_label = None
-    item_ingredients = None
-    pack = None
+    time.sleep(GEN_TIMEOUT)
+    ID = f'{ind}-{index}-{aisle.upper()[:3]}'
+    Region = None
+    City = None
 
-    new_row = {'idx': itemIdx,
-               'name': name, 'brand': brand,
-               'aisle': aisle, 'subaisle': subaisle,
-               'subsubaisle': subsubaisle,
-               'size': size, 'price': price, 'multi_price': multi_price,
-               'old_price': old_price, 'pricePerUnit': pricePerUnit,
-               'itemNum': itemNum, 'description': description, 'serving': serving,
-               'img_urls': ', '.join(img_urls), 'item_label': item_label,
-               'item_ingredients': item_ingredients, 'url': item_url,
-               'pack': pack,
-               'timeStamp': datetime.datetime.now(pytz.timezone('US/Eastern')).isoformat()}
-    return (new_row)
+    ProductName = None
+    ProductBrand = None
+    ProductCategory = None
+    ProductSubCategory = None
 
+    ProductImages = None
+    Description = None
+    Price = None
+    Nutr_label = None
+    Ingredients = None
+    Unitpp = None
+    Netcontent_val = None
+    Netcontent_org = None
+    Netcontent_unit = None
+    NutrInfo_org = 'N/A'
+    Servsize_container_type_org = 'N/A'
+    Servsize_container_type_val = 'N/A'
+    Servsize_container_type_unit = 'N/A'
+    Servsize_portion_org = None
+    Servsize_portion_val = None
+    Servsize_portion_unit = None
+    Servings_cont = None
+    Containersize_org = None
+    Containersize_val = None
+    Containersize_unit = None
+    Packsize_org = None
+    Pack_type = None
+    ProductVariety = None
+    ProductFlavor = None
+    Cals_org_pp = None
+    Cals_value_pp = None
+    Cals_unit_pp = None
+    TotalCarb_g_pp = None
+    TotalCarb_pct_pp = 'N/A'
+    TotalSugars_g_pp = None
+    TotalSugars_pct_pp = 'N/A'
+    AddedSugars_g_pp = 'N/A'
+    AddedSugars_pct_pp = 'N/A'
+    Cals_value_p100g = 'N/A'
+    Cals_unit_p100g = 'N/A'
+    TotalCarb_g_p100g = 'N/A'
+    TotalCarb_pct_p100g = 'N/A'
+    TotalSugars_g_p100g = 'N/A'
+    TotalSugars_pct_p100g = 'N/A'
+    AddedSugars_g_p100g = 'N/A'
+    AddedSugars_pct_p100g = 'N/A'
+    UPC = None
+    Notes = 'OK'
 
-def getItem_urls(driver, EXPLICIT_WAIT_TIME):
-    None
+    global LOCATION
+    print(LOCATION)
+
+    new_row = {
+        'ID': ID,
+        'Country': 'United States',
+        'Store': 'Target',
+        'Region': Region,
+        'City': City,
+        'ProductName': ProductName,
+        'ProductVariety': ProductVariety,
+        'ProductFlavor': ProductFlavor,
+        'Unitpp': Unitpp,
+        'ProductBrand': ProductBrand,
+        'ProductAisle': aisle,
+        'ProductCategory': ProductCategory,
+        'ProductSubCategory': ProductSubCategory,
+        'ProductImages': ProductImages,
+        'Containersize_org': Containersize_org,
+        'Containersize_val': Containersize_val,
+        'Containersize_unit': Containersize_unit,
+        'Cals_org_pp': Cals_org_pp,
+        'Cals_value_pp': Cals_value_pp,
+        'Cals_unit_pp': Cals_unit_pp,
+        'TotalCarb_g_pp': TotalCarb_g_pp,
+        'TotalCarb_pct_pp': TotalCarb_pct_pp,
+        'TotalSugars_g_pp': TotalSugars_g_pp,
+        'TotalSugars_pct_pp': TotalSugars_pct_pp,
+        'AddedSugars_g_pp': AddedSugars_g_pp,
+        'AddedSugars_pct_pp': AddedSugars_pct_pp,
+        'Cals_value_p100g': Cals_value_p100g,
+        'Cals_unit_p100g': Cals_unit_p100g,
+        'TotalCarb_g_p100g': TotalCarb_g_p100g,
+        'TotalCarb_pct_p100g': TotalCarb_pct_p100g,
+        'TotalSugars_g_p100g': TotalSugars_g_p100g,
+        'TotalSugars_pct_p100g': TotalSugars_pct_p100g,
+        'AddedSugars_g_p100g': AddedSugars_g_p100g,
+        'AddedSugars_pct_p100g': AddedSugars_pct_p100g,
+        'Packsize_org': Packsize_org,
+        'Pack_type': Pack_type,
+        'Netcontent_val': Netcontent_val,
+        'Netcontent_org': Netcontent_org,
+        'Netcontent_unit': Netcontent_unit,
+        'Price': Price,
+        'Description': Description,
+        'Nutr_label': Nutr_label,
+        'Ingredients': Ingredients,
+        'NutrInfo_org': NutrInfo_org,
+        'Servsize_container_type_org': Servsize_container_type_org,
+        'Servsize_container_type_val': Servsize_container_type_val,
+        'Servsize_container_type_unit': Servsize_container_type_unit,
+        'Servsize_portion_org': Servsize_portion_org,
+        'Servsize_portion_val': Servsize_portion_val,
+        'Servsize_portion_unit': Servsize_portion_unit,
+        'Servings_cont': Servings_cont,
+        'ProdType': 'N/A',
+        'StorType': 'N/A',
+        'ItemNum': 'N/A',
+        'SKU': 'N/A',
+        'UPC': UPC,
+        'url': item_url,
+        'DataCaptureTimeStamp': datetime.datetime.now(pytz.timezone('US/Eastern')).isoformat(),
+        'Notes': Notes
+    }
+    return new_row
 
